28814646ce0283a08ed3435579034400871ee0a8
TA-800 <taherali0905@gmail.com>
2025-10-12 21:55:04 -0400

    colorscheme and minor fzf change

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -2,9 +2,18 @@ return {
 	url = "https://github.com/ibhagwan/fzf-lua",
 	config = function()
 		local fzf = require("fzf-lua")
-		fzf.setup({ "telescope" })
+		fzf.setup({
+			"telescope",
+			files = {
+				formatter = "path.filename_first",
+			},
+		})
 		vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
 		vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
+		-- Shortcut for searching for your Neovim configuration files
+		vim.keymap.set("n", "<leader>sn", function()
+			fzf.files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
