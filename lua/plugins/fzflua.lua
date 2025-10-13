fd93f7bf3449c74e19ad5014aa5a094f53f0b50b
TA-800 <taherali0905@gmail.com>
2025-10-12 20:34:49 -0400

    add fzf mappings

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -1,6 +1,9 @@
 return {
 	url = "https://github.com/ibhagwan/fzf-lua",
-	config = function() 
-		require('fzf-lua').setup {} 
-	end
+	config = function()
+		local fzf = require("fzf-lua")
+		vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "[ ] Find existing buffers" })
+	end,
 }
